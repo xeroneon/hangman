857ae5f859ada52115bd07fea01127c761f42e6f
Xeroneon <andrewvasquez0113@gmail.com>
2018-05-15 18:25:21 -0700

    took out unused code and commented out console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,28 +19,27 @@ var losses = 0;
 
 //function to take a random word from word list and use it for current word
 var currentWord = words[Math.floor(Math.random() * words.length)];
-console.log(currentWord);
+//console.log(currentWord);
 
 //function to get the letter that has been pressed
 document.onkeyup = function (event) {
     //captures keypress and converts to lowercase and stores as variable
     letterPress = event.key.toLowerCase();
-    console.log("letterPress", letterPress);
+    // console.log("letterPress", letterPress);
     //checks to see if letter matches any letters in current word
     if (currentWord.match(letterPress)) {
-        console.log("match");
+        // console.log("match");
         //sets i to index of letter pressed
         var i = currentWord.indexOf(letterPress);
         //changes html to be the letter that was pressed
         document.getElementById(i).textContent = letterPress;
-        // if ()
+        
         for (k = 0; k < 7; k++) {
             //if letter is matched then it will place the letter in the right place in an array for comparison
             guessedWord.splice(k, 1, document.getElementById(k).textContent);
-            console.log("guessedWord", guessedWord);
+            // console.log("guessedWord", guessedWord);
 
         }
-        // console.log("guessedwordstring", guessedWord.join(""));
         //use .join to make array a string with no commas and if they match then alert win and reset board
         if (guessedWord.join("") === currentWord) {
             for (j = 0; j < 7; j++) {
@@ -56,11 +55,6 @@ document.onkeyup = function (event) {
             document.getElementById("wins").textContent = wins;
             alert("you won!");
         }
-        // guessedWord = guessedWord + letterPress;
-        // console.log("guessedWord", guessedWord);
-        // if (currentWord.length===guessedWord.length && currentWord.split("").sort().join() == guessedWord.split("").sort().join()) {
-        //     alert("you won");
-        // }
 
     }
 
@@ -72,8 +66,8 @@ document.onkeyup = function (event) {
             //score goes down by 1
             guessRemain--;
             document.getElementById("guess-remain").textContent = guessRemain;
-            console.log("guessRemain", guessRemain);
-            console.log("guessLetters", guessLetters);
+            // console.log("guessRemain", guessRemain);
+            // console.log("guessLetters", guessLetters);
 
 
             //if score is zero reset board
